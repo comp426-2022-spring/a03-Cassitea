154997340fef8cc14a16d2e77bb92f01ed36253f
Cassitea <cassidynguyen3@gmail.com>
2022-04-24 18:11:45 -0400

    created my port variable, HTTP_PORT

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 
 const args = require('minimist')(process.argv.slice(2));
 args['port']
+const HTTP_PORT = args.port || 5000
 
 // Start an app server
 const server = app.listen(HTTP_PORT, () => {
