3d6cc202807157e89e78533f6204fcfbc4246e81
Cassitea <cassidynguyen3@gmail.com>
2022-04-24 18:33:24 -0400

    simplified endpoint code for the given OK and 404

--- server.js
+++ server.js
@@ -12,22 +12,32 @@ const server = app.listen(HTTP_PORT, () => {
 });
 
 // Default response for any other request
+/*
 app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
-});
+}); */
 
-app.get('/app/', (req, res) => {
+/*app.get('/app/', (req, res) => {
     // Respond with status 200
         res.statusCode = 200;
     // Respond with status message "OK"
         res.statusMessage = 'OK';
         res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
         res.end(res.statusCode+ ' ' +res.statusMessage)
-    });
+    });*/
 
+// TODO: Endpoints
 
+// Simplified of the given one
+app.get('/app/', (req, res) => {
+    res.status(200).end('OK')
+    res.type('text/plain')
+});
 
-
+app.use(function (req, res) {
+    res.status(404).end('404 NOT FOUND')
+    res.type('text/plain')
+})
 
 
 
